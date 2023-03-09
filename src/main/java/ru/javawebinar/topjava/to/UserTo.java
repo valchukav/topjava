da965158f5bdfcbf87afa1c5bd9ee28221dab0c2
Alexei Valchuk <a.valchukav@gmail.com>
2023-03-09 16:32:38 +0300

    feat: provide validation for UserTo

--- src/main/java/ru/javawebinar/topjava/to/UserTo.java
+++ src/main/java/ru/javawebinar/topjava/to/UserTo.java
@@ -2,6 +2,10 @@ package ru.javawebinar.topjava.to;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * @author Alexei Valchuk, 08.03.2023, email: a.valchukav@gmail.com
  */
@@ -12,11 +16,18 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 public class UserTo extends BaseTo{
-    
+
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String name;
 
+    @Email
+    @NotBlank
+    @Size(max = 100)
     private String email;
 
+    @NotBlank
+    @Size(min = 5, max = 32, message = "length must be between 5 and 32 characters")
     @ToString.Exclude
     private String password;
 
